4e92435b035aa24bf15e47cfae4254b61b9c65cf
smallCodeWangzh <34759615+smallCodeWangzh@users.noreply.github.com>
2020-04-16 16:22:41 +0800

    wangzh:增加钉钉以及DockerMaven配置

--- src/main/java/com/briup/organization/SmsOrganizationApplication.java
+++ src/main/java/com/briup/organization/SmsOrganizationApplication.java
@@ -1,11 +1,13 @@
 package com.briup.organization;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.briup.organization.mapper")
+@EnableSwagger2Doc
 public class SmsOrganizationApplication {
 
     public static void main(String[] args) {
